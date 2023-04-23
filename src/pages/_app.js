468a0bfa91b6376770becd209ec4f4227d08bee9
jashAtgit <jash.sai33@gmail.com>
2023-04-23 15:02:19 +0530

    removed msw mocking instances

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,21 +1,10 @@
-import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
-import { useEffect } from 'react';
 import "./styles/styles.css"
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-import '@fortawesome/fontawesome-free/css/all.css'
-config.autoAddCss = false
 
 export default function App(props) {
   const { Component, pageProps } = props;
 
-  // useEffect(() => {
-  //   const { worker } = require('./mocks/browser')
-  
-  //   worker.start()
-  //   }, []);
 
   return (
     <>
@@ -28,7 +17,6 @@ export default function App(props) {
         withGlobalStyles
         withNormalizeCSS
         theme={{
-          /** Put your mantine theme override here */
           colorScheme: 'light',
         }}
       >
--- src/pages/mocks/browser.js
+++ src/pages/mocks/browser.js
@@ -1,12 +1,12 @@
-// src/mocks/browser.js
-import { setupWorker } from 'msw'
-import { setupServer } from 'msw/node'
-import { handlers } from './handlers'
+// // src/mocks/browser.js
+// import { setupWorker } from 'msw'
+// import { setupServer } from 'msw/node'
+// import { handlers } from './handlers'
 
-const server = setupServer(...handlers)
+// const server = setupServer(...handlers)
 
-// start the server
-server.listen()
+// // start the server
+// server.listen()
 
-// This configures a Service Worker with the given request handlers.
-export const worker = setupWorker(...handlers)
+// // This configures a Service Worker with the given request handlers.
+// export const worker = setupWorker(...handlers)
